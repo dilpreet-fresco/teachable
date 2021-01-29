773c3cd6bfb0e27929f1b248676105971d309e77
Dilpreet Singh <pardeep32732@gmail.com>
2021-01-29 15:07:56 +0530

    Proxy Added

--- src/modules/admin/auth/authService.js
+++ src/modules/admin/auth/authService.js
@@ -1,8 +1,4 @@
-import ax from 'axios';
-
-const axios = ax.create({
-  baseURL: 'http://13.232.126.227:5000',
-});
+import axios from 'axios';
 
 export default class AuthService {
   static async signinWithEmailAndPassword({ email, password }) {
